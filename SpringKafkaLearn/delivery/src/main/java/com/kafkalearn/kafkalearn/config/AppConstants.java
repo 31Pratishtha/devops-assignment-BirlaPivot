7212ee4627c33124d3bfea3481827ea6122c783a
Pratishtha <96373767+31Pratishtha@users.noreply.github.com>
2025-01-31 15:00:05 +0530

    Delete SpringKafkaLearn directory

--- SpringKafkaLearn/delivery/src/main/java/com/kafkalearn/kafkalearn/config/AppConstants.java
+++
@@ -1,5 +0,0 @@
-package com.kafkalearn.kafkalearn.config;
-
-public class AppConstants {
-    public static final String LOCATION_TOPIC_NAME = "location-update";
-}
--- SpringKafkaLearn/enduser/src/main/java/com/kafkalearn/kafkalearn/config/AppConstants.java
+++
@@ -1,6 +0,0 @@
-package com.kafkalearn.kafkalearn.config;
-
-public class AppConstants {
-    public static final String LOCATION_UPDATE_TOPIC = "location-update";
-    public static final String GROUP_ID = "";
-}
